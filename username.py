ae16ff14d69a4e39619da6973378ec237bcf0a22
Harry Gardiner <harry97uk@hotmail.co.uk>
2023-10-31 17:36:35 +0000

    username cleanup

--- username.py
+++ username.py
@@ -1,17 +1,9 @@
 # Function to handle username recognition
-import requests
-
-def penguin():
-    response = requests.get("https://api.getpenguin.com/check/services/details")
-    data = response.json()
-    print(data)
-
 
 
 def recognize_username(username):
     # Implement username recognition and social network check logic here
     # Sample logic:
-    penguin()
 
     social_networks = {
         "Facebook": "yes",
